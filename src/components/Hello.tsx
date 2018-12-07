e05a8b5da6e563fe12c84fb20f81216bc2e39d96
Дмитрий Ромашов <framebassman@gmail.com>
2018-12-07 19:36:14 +0300

    fix styles

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -1,10 +1,10 @@
 import * as React from 'react';
-import './Hello.scss';
+const styles = require('./Hello.scss');
 
 export interface HelloProps { compiler: string; framework: string; }
 
 export const Hello = (props: HelloProps) => (
-    <div className="hello">
+    <div className={styles.hello}>
       <h1>Hello from {props.compiler} and {props.framework}!</h1>
     </div>
   );
