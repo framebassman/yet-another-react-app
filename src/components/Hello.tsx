794732f397d2c1b5afe73826277ca5831de0a562
Дмитрий Ромашов <framebassman@gmail.com>
2018-12-07 19:13:05 +0300

    init

---
+++ src/components/Hello.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import './Hello.scss';
+
+export interface HelloProps { compiler: string; framework: string; }
+
+export const Hello = (props: HelloProps) => (
+    <div className="hello">
+      <h1>Hello from {props.compiler} and {props.framework}!</h1>
+    </div>
+  );
