22f25ded57d43314155e2bb192553d6c2a8e0704
Дмитрий Ромашов <framebassman@gmail.com>
2018-12-13 09:12:31 +0300

    add nodemon

--- server/main.ts
+++ server/main.ts
@@ -1,23 +1,28 @@
 import * as path from 'path';
 import * as express from 'express';
+import {Request, Response} from 'express';
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, '../public')));
 
-app.get('/customer_agreement', (req: express.Request, res: express.Response) => {
+app.get('/healthcheck', (req: Request, res: Response) => {
+    res.sendStatus(200);
+});
+
+app.get('/customer_agreement', (req: Request, res: Response) => {
     res.setHeader('Content-Type', 'application/pdf');
     res.sendFile(path.join(__dirname, '../public/files/agreement.pdf'));
 });
 
 /*
-app.get('/privacy_policy', (req: express.Request, res: express.Response) => {
+app.get('/privacy_policy', (req: Request, res: Response) => {
     res.setHeader('Content-Type', 'application/pdf');
     res.sendFile(path.join(__dirname, '../public/files/policy.pdf'));
 });
 */
 
-app.get('/', (req: express.Request, res: express.Response) => {
+app.get('/', (req: Request, res: Response) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
